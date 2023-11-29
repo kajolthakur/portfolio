b841176fcdf3a006af2a1911ea989cd174ce6803
kajolthakur <thakur.kajol18@gmail.com>
2023-11-29 12:19:32 +0530

    final commit

--- Ecommerce_project/java.js
+++ Ecommerce_project/java.js
@@ -14,39 +14,3 @@ menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navmenu.classList.toggle('open');
 }
-
-(function () {
-    emailjs.init("EwgM4dAtmMIi8cI7Z");
-})();
-
-
-function sendMail(e) {
-    e.preventDefault();
-    const name = document.getElementById('name').value
-    const email = document.getElementById('email').value
-    const number = document.getElementById('number').value
-    const subject = document.getElementById('subject').value
-    const message = document.getElementById('message').value
-
-    const mailMessage = `Hey, my name is ${name}, email is ${email} , number is ${number} , `
-    let templateParams = {
-        subject: subject,
-        from_name: name,
-        to_name: "Kajol Thakur",
-        message: `${message}
-         Here are my details =  ${mailMessage}
-        `
-    }
-
-    emailjs.send("service_1d1vgsb", "template_wix7plp", templateParams, "EwgM4dAtmMIi8cI7Z").then(function (response) {
-        console.log('SUCCESS!', response.status, response.text);
-        window.scrollTo(0, 0)
-        document.getElementById('name').value = ""
-        document.getElementById('email').value = ""
-        document.getElementById('number').value = ""
-        document.getElementById('subject').value = ""
-        document.getElementById('message').value = ""
-    }, function (error) {
-        console.log('FAILED...', error);
-    });
-}
--- portfolio.js
+++ portfolio.js
@@ -7,7 +7,7 @@ menuIcon.onclick = () => {
 }
 
 let sections = document.querySelectorAll('section');
-let navlinks =  document.querySelectorAll('header nav a');
+let navlinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
     sections.forEach(sec => {
@@ -16,7 +16,7 @@ window.onscroll = () => {
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
-        if(top >= offset && top < offset + height){
+        if (top >= offset && top < offset + height) {
             navlinks.forEach(links => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
@@ -32,6 +32,43 @@ window.onscroll = () => {
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
 
-   
 
+
+}
+
+
+(function () {
+    emailjs.init("EwgM4dAtmMIi8cI7Z");
+})();
+
+
+function sendMail(e) {
+    e.preventDefault();
+    const name = document.getElementById('name').value
+    const email = document.getElementById('email').value
+    const number = document.getElementById('number').value
+    const subject = document.getElementById('subject').value
+    const message = document.getElementById('message').value
+
+    const mailMessage = `Hey, my name is ${name}, email is ${email} , number is ${number} , `
+    let templateParams = {
+        subject: subject,
+        from_name: name,
+        to_name: "Kajol Thakur",
+        message: `${message}
+         Here are my details =  ${mailMessage}
+        `
+    }
+
+    emailjs.send("service_1d1vgsb", "template_3g2ld09", templateParams, "EwgM4dAtmMIi8cI7Z").then(function (response) {
+        console.log('SUCCESS!', response.status, response.text);
+        window.scrollTo(0, 0)
+        document.getElementById('name').value = ""
+        document.getElementById('email').value = ""
+        document.getElementById('number').value = ""
+        document.getElementById('subject').value = ""
+        document.getElementById('message').value = ""
+    }, function (error) {
+        console.log('FAILED...', error);
+    });
 }
